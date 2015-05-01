e52b595fac5729f4bc9cfa7cb8c97ed50c4f76dc
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2015-05-01 23:02:50 +0200

    Cleanup in import hook code

--- pyrton/_import_hook.py
+++ pyrton/_import_hook.py
@@ -11,31 +11,26 @@ class PyrsistentImporter(object):
         self._cache = {}
 
     def module_matches(self, name):
+        # TODO: Support regex matching
         return name in self._match_expressions
 
     def add_matchers(self, matchers):
         self._match_expressions.extend(matchers)
 
     def find_module(self, name, path=None):
-        if not self.module_matches(name):
-            return None
+        if self.module_matches(name):
+            return self
 
-        # TODO: Is all this caching stuff really needed or could we
-        #       just move this code the load_module?
+        return None
+
+    def load_module(self, name):
         try:
             suffix = name.split('.')[-1]
-            self._cache[name] = imp.find_module(suffix)
+            fd, pathname, (suffix, mode, type_) = imp.find_module(suffix)
         except ImportError:
             return None
 
-        return self
-
-    def load_module(self, name):
-        print "Called load %s" % name
-
         module = types.ModuleType(name) #create empty module object
-        fd, pathname, (suffix, mode, type_) = self._cache[name]
-        print "Called load %s" % ((fd, pathname, (suffix, mode, type_)),)
 
         with fd:
             if type_ == imp.PY_SOURCE:
@@ -46,19 +41,17 @@ class PyrsistentImporter(object):
                 filename = os.path.join(pathname, '__init__.py')
                 module.__path__ = [pathname]
             else:
-                return imp.load_module(name, fd, pathname,
-                                       (suffix, mode, type_))
-            if not filename == pathname:
+                return imp.load_module(name, fd, pathname, (suffix, mode, type_))
+
+            if filename != pathname:
                 try:
-                    with open(filename, 'U') as realfile:
-                        src = realfile.read()
+                    with open(filename, 'U') as real_file:
+                        src = real_file.read()
                 except IOError: #fallback
-                    return imp.load_module(name, fd, pathname,
-                                           (suffix, mode, type_))
+                    return imp.load_module(name, fd, pathname, (suffix, mode, type_))
             else:
                 src = fd.read()
 
-        module = types.ModuleType(name)
         module.__file__ = filename
         inlined = transform(src)
         code = compile(inlined, filename, 'exec')
@@ -66,9 +59,6 @@ class PyrsistentImporter(object):
         exec(code,  module.__dict__)
 
         return module
-
-# TODO: Initialization function that inserts itself in meta_path if not already present and then
-#       registers regexps that are used to match which modules that should be affected which calls
 
 importer = PyrsistentImporter()
 
---
+++ run_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+PYTHONPATH=. python tests
